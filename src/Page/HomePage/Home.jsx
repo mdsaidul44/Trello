f72beb2f6e2a74c2fe575be9a5d1de551e24ce37
mdsaidul44 <islam88758@gmail.com>
2025-02-06 21:24:14 +0600

    home section added

--- src/Page/HomePage/Home.jsx
+++ src/Page/HomePage/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Trello</h1>
+        <div className='m-20'>
+            <h1 className='text-7xl mb-4 underline text-teal-600'>Trello</h1>
+            <p className='w-[700px] text-lg'>Trello is a visual project management tool that uses a Kanban-style board system to help individuals and teams organize tasks, collaborate, and track progress efficiently. It allows users to create boards, lists, and cards to represent projects, workflow stages, and individual tasks. With features like task assignments, due dates, labels, attachments, and integrations with other productivity tools, Trello simplifies project planning and enhances team collaboration.</p>
         </div>
     );
 };
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -15,9 +15,9 @@ const Card = () => {
                 <h1 className='text-5xl  font-bold text-cyan-700'>Trello Short</h1>
                 <p className='mt-2 font-serif'>Trello is a visual project management tool that helps teams organize tasks, collaborate,<br /> and track progress using boards, lists, and cards.</p>
             </div>
-            <div className='grid  grid-cols-3 gap-10 lg:ml-20'>
+            <div className='grid  grid-cols-3 gap-10 mb-20 lg:ml-20'>
             {
-                item.map(data=>  <div className="card font-serif bg-base-100 w-96 shadow-xl">
+                item.map(data=>  <div className="card hover:shadow-teal-900  font-serif bg-base-100 w-96 shadow-xl">
                     <figure>
                         <img
                             src={data.image}
