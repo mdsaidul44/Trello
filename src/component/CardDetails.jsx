f2bc957d7f254e1ffba8666031dcfb068252d81c
mdsaidul44 <islam88758@gmail.com>
2025-02-07 10:45:08 +0600

    card details data set done

--- src/component/CardDetails.jsx
+++ src/component/CardDetails.jsx
@@ -7,18 +7,19 @@ const CardDetails = () => {
     const cards = cardDetails.find(card => card.id == id)
     console.log('Data is here', cards)
     return (
-        <div className='h-screen'>
-            <div class="card lg:card-side bg-base-100 shadow-xl">
+        <div className='h-screen p-32'>
+            <div class="card  lg:card-side bg-base-100 shadow-xl">
                 <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+                    <img className='h-72'
+                        src={cards.image }
                         alt="Album" />
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
+                <div class="card-body font-serif">
+                    <h2 class="card-title">Title: {cards.title}</h2>
+                    <h2 class="card-title">{cards.name}</h2>
+                    <p>{cards.short_description}</p>
                     <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Listen</button>
+                        <button class="btn btn-primary">Delete</button>
                     </div>
                 </div>
             </div>
